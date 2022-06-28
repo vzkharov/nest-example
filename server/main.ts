7873fd7f6f112ab1c0457e1c847c11942afbe97f
zkharov <zakharovvadzim@yandex.by>
2022-06-28 17:45:31 +0300

    BACKUP part 3

--- server/main.ts
+++ server/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory, NestApplication } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import helmet from 'helmet'
 
 import { GlobalFilter } from '~/common/filters/global.filter'
@@ -18,13 +19,26 @@ async function bootstrap() {
     app.useGlobalInterceptors(new GlobalInterceptor())
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
 
-    app.use(helmet())
-
     app.enableCors({
-        origin: '*',
+        origin: true,
+        allowedHeaders: '*',
     })
 
-    await app.listen(port || 3000)
+    app.use(helmet())
+
+    app.setGlobalPrefix('api')
+
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Transfers API with Authorization')
+        .setDescription('Transfers API')
+        .setVersion('1.0')
+        .addTag('transfer')
+        .build()
+
+    const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig)
+    SwaggerModule.setup('swagger', app, swaggerDocument)
+
+    await app.listen(port || 8080)
 }
 
 void bootstrap()
